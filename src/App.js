2ef90eb8d6a1ffa7c41deffbc8451fce130fca2e
JamesJan <martin750904@gmail.com>
2021-03-02 16:42:58 +0800

    update files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import files from './files.json'
+import './style.css'
+import FileNode from './FileNode'
+class App extends Component {
+    render() {
+        return (
+            <div>
+                {/* <FileNode name={files.name} files={files.files} /> */}
+                {/* same as below */}
+                <FileNode {...files} />
+                {/* 表示把所有files的屬性 全部丟到FileNode裡當他的props */}
+            </div>
+        );
+    }
 }
 
 export default App;
